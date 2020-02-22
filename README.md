0be5d8a83f025a9a84d632df74abee56d262320a
Artin <lengthmin@gmail.com>
2020-02-22 20:46:11 +0800

    Add format_name.py

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 <http://cas.swust.edu.cn/authserver/captcha>
 
 各目录为某时标注的验证码数据
-验证码文件名格式为：`XXXX_文件hash值.jpg`
+验证码文件名格式为：`XXXX_文件md5值.jpg`
 其中 `XXXX` 代表该图片的验证码。
 
 数据集大小：
---
+++ scripts/format_name.py
@@ -0,0 +1,23 @@
+import re
+import os
+import hashlib
+
+# 图片路径
+root = r"./temp"
+all_files = os.listdir(root)
+
+for file in all_files:
+    old_path = os.path.join(root, file)
+
+    # 已被修改过忽略
+    if len(file.split(".")[0]) > 32:
+        continue
+
+    # 采用标注_文件md5码.图片后缀 进行命名
+    with open(old_path, "rb") as f:
+        _id = hashlib.md5(f.read()).hexdigest()
+    new_path = os.path.join(root, file.replace(".", "_{}.".format(_id)))
+
+    # 重复标签的时候会出现形如：abcd (1).jpg 这种形式的文件名
+    new_path = re.sub(r" \(\d+\)", "", new_path)
+    os.rename(old_path, new_path)
